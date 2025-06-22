3600dcce4a5ce9d1ba07eee79592db71198df13a
Derbeli Iyed <derbaliiyed@gmail.com>
2025-06-23 00:46:36 +0100

    First Steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
+import ProjectManagerView from './views/ProjectManagerView';
+import DomainManagerView from './views/DomainManagerView';
+import DeveloperView from './views/DeveloperView';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+        <nav>
+          <Link to="/project-manager">Project Manager</Link>
+          <Link to="/domain-manager">Domain Manager</Link>
+          <Link to="/developer">Developer</Link>
+        </nav>
+      <Routes>
+        <Route path="/project-manager" element={<ProjectManagerView />} />
+        <Route path="/domain-manager" element={<DomainManagerView />} />
+        <Route path="/developer" element={<DeveloperView />} />
+      </Routes>
+    </Router>
+        
   );
 }
 
---
+++ src/views/DomainManagerView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DomainManagerView() {
+  return (
+    <div>DomainManagerView</div>
+  )
+}
