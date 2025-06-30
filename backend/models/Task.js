eb5d8b4085fe7c940af8e701a4c2d4822e2ea091
DERBALI Iyed <iyed.derbali@soprahr.com>
2025-06-30 16:22:41 +0100

    Task Management API

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -15,7 +15,7 @@ const todoSchema = new mongoose.Schema(
             dueDate: { type: Date, required: true },
             assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
             createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-            attachements: [{ type: String }],
+            attachments: [{ type: String }],
             todoChecklist: [todoSchema],
             progress: { type: Number, default: 0 }
         },{timestamps:true});
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-// const taskRoutes = require("./routes/tasks");
+const taskRoutes = require("./routes/taskRoutes");
 // const reportRoutes = require("./routes/reports");
 
 const app = express();
@@ -29,7 +29,7 @@ app.use(express.json());
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);  
-// app.use("/api/tasks", taskRoutes);
+app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
 
 
