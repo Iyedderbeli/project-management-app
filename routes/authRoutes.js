dc9e86bc1e0d3b68d4fb8b0f4883422ad1751111
Derbeli Iyed <146973943+Iyedderbeli@users.noreply.github.com>
2025-07-05 01:30:41 +0100

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { registerUser, loginUser, getProfile, updateProfile } = require('../controllers/authController');
+const router = express.Router();
+const { protect } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware'); // Import the upload middleware
+
+
+//Auth Routes
+router.post('/register', registerUser); // Register user
+router.post('/login', loginUser); // Login user
+router.get('/profile', protect, getProfile); // Get user profile
+router.put('/profile', protect, updateProfile); // Update user profile
+
+router.post("/upload-image", upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl });
+
+})
+
+module.exports = router;
