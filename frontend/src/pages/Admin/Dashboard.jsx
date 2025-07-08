c3236bbacba2fea8010003e8e4320b26fbb48311
DERBALI Iyed <iyed.derbali@soprahr.com>
2025-07-08 15:36:54 +0100

    NavBar

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { UserContext } from '../../context/userContext';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 
 const Dashboard = () => {
   useUserAuth();
   
   const {user}=useContext(UserContext)
 
-  return (
-    <div>Dashboard</div>
-  )
+  return <DashboardLayout>Dashboard</DashboardLayout>;
 }
 
 export default Dashboard
