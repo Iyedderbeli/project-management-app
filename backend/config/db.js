392fc648a00c57e2d759da9de3bf8d5118e1b487
DERBALI Iyed <iyed.derbali@soprahr.com>
2025-07-08 13:51:00 +0100

    SignUp + Login

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("MongoDB connected");
+        
+    } catch (error) {
+        console.log("MongoDB connection failed:", error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const router = express.Router();
+const { getDashboardData, getUserDashboardData, getTasks, getTasksById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist } = require("../controllers/taskController");
+
+
+// Task Management Routes
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("/user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks); // Get all tasks (Admin: all, User: assigned)
+router.get("/:id", protect, getTasksById); // Get task by ID
+router.post("/", protect, adminOnly, createTask); // Create a task (Admin only)
+router.put("/:id", protect, updateTask); // Update task details
+router.delete("/:id", protect, adminOnly, deleteTask); // Delete a task (Admin only)
+router.put("/:id/status", protect, updateTaskStatus); // Update task status
+router.put("/:id/todo", protect, updateTaskChecklist); // Update task checklist
+
+module.exports = router;
+
+
+const app = express();
